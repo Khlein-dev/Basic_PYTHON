232639d164777bcc0efd4b0a9ba0bad7644ea892
Khlein-dev <kierjerymiepascua@gmail.com>
2025-08-29 23:59:13 +0800

    Day 2 practice pygame

--- pyGame.py
+++ pyGame.py
@@ -4,12 +4,15 @@ from sys import exit
 pygame.init()
 screen = pygame.display.set_mode((800, 400))
 pygame.display.set_caption('Khlein GO! GO!')
-icon = pygame.image.load('khlein.png')
+icon = pygame.image.load('pics/khlein.png')
 pygame.display.set_icon(icon)
 clock = pygame.time.Clock()
 
-test_surface = pygame.Surface((100, 200))
-test_surface.fill('Red')
+# test_surface = pygame.Surface((100, 200))
+# test_surface.fill('Red')
+
+sky_surface = pygame.image.load('pics/sky.jpg')
+ground_surface = pygame.image.load('pics/ground.jpg')
 
 while True:
     for event in pygame.event.get():
@@ -17,7 +20,9 @@ while True:
             pygame.quit()
             exit()
             
-    screen.blit(test_surface,(200,100))
+    # screen.blit(sky_surface,(0,0))
+    screen.blit(pygame.transform.scale(sky_surface, (800, 400)), (0, 0))
+    screen.blit(pygame.transform.scale(ground_surface, (800, 100)), (0, 300))
     
     #Draw elements
     #Update everything
