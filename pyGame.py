d584c5e72605b6668481ed80ef68c1600d54bce7
Khlein-dev <kierjerymiepascua@gmail.com>
2025-09-02 16:39:28 +0800

    Day 4 practice pygame

--- pyGame.py
+++ pyGame.py
@@ -14,7 +14,9 @@ test_font = pygame.font.Font('pics/stocky.ttf', 50)
 
 sky_surface = pygame.image.load('pics/sky.jpg').convert()
 ground_surface = pygame.image.load('pics/ground.jpg').convert()
-text_surface = test_font.render('Khlein GO! GO!', False, 'Blue')
+
+text_surface = test_font.render('Khlein GO! GO!', False, (64, 64, 64))
+text_rect =text_surface.get_rect(center = (400, 50))
 
 doggy_surface = pygame.image.load('pics/doggy.png').convert_alpha()
 doggy_surface = pygame.transform.scale(doggy_surface, (90, 90))
@@ -23,28 +25,61 @@ doggy_rect = doggy_surface.get_rect(midbottom = (600, 340))
 
 player_surface = pygame.image.load('pics/player.png').convert_alpha()
 player_surface = pygame.transform.scale(player_surface, (120, 120))
-player_rect = player_surface.get_rect(midbottom = (50, 340))
+player_rect = player_surface.get_rect(midbottom = (100, 340))
+player_rect.inflate_ip(-30, -20)
 
 while True:
     for event in pygame.event.get():
         if event.type ==pygame.QUIT:
             pygame.quit()
             exit()
+        # if event.type == pygame.MOUSEMOTION:
+        #   if player_rect.collidepoint(event.pos): print('collision')
+        
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_SPACE:
+                print('jump')
+            
+        if event.type == pygame.KEYUP:
+             print("key up")
             
     # screen.blit(sky_surface,(0,0))
     screen.blit(pygame.transform.scale(sky_surface, (800, 400)), (0, 0))
     screen.blit(pygame.transform.scale(ground_surface, (800, 100)), (0, 300))
-    screen.blit(text_surface,(200, 50))
+    
+    # Draw Line
+    # pygame.draw.line(screen, 'gold', (0,0) , pygame.mouse.get_pos(), 6)
+    
+    # Draw Circle
+    pygame.draw.ellipse(screen, "#c0e8ec", pygame.Rect(50,200,100,125))
+    pygame.draw.rect(screen, "#c0e8ec" , text_rect, 6, 20)
+    
+    screen.blit(text_surface,text_rect)
     # doggy_sit -= 2
     # if doggy_sit < -100: doggy_sit = 800
+    
     
     doggy_rect.x -=2
     if doggy_rect.right < 0:
          doggy_rect.left = 800
     screen.blit(doggy_surface, doggy_rect)
     
-    player_rect.left += 1
+    # player_rect.left += 1
+    
     screen.blit(player_surface, player_rect)
+    
+    # if player_rect.colliderect(doggy_rect):
+    #     print("collide")
+    
+    # mouse_pos = pygame.mouse.get_pos()
+    # if player_rect.collidepoint((mouse_pos)):
+    #     print(pygame.mouse.get_pressed())
+
+    # keys = pygame.key.get_pressed()
+    # if keys[pygame.K_SPACE]:
+    #     print('jump')
+    
+    
     
     #Draw elements
     #Update everything
